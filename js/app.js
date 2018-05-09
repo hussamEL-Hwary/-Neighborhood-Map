fe6a68aa0435c0f671707bbaac011987b4054017
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2018-05-09 23:54:17 +0200

    feat: init map

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+/* create map */
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 29.979322, lng: 31.132781},
+    zoom: 15
+  });
+  ko.applyBindings(modelView);
+}
+
 /* open and hide side bar */
 function openNav() {
   $('.upper-nav').css("margin-left", "19%");
